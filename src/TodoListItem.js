90eaf2b121ff79680680f8cc4f4cfaa8a96cdbad
meriemrezkallah <meriem.rezkallah3010@gmail.com>
2022-05-17 11:40:09 -0500

    delet console.log

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 function TodoListItem (props) {
-    console.log("herereee 1");
-    console.log(props.id);
-    console.log("herereee 2");
+ 
 return ( 
     <> 
     
